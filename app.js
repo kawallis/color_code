cb3312cb7423eb44a201dc7312304298e52349c1
Chris_Wallace <kawallis@icloud.com>
2017-02-15 19:08:00 -0800

    made the game work by making a few new functions

--- app.js
+++ app.js
@@ -1,18 +1,54 @@
 'use strict';
 var header = document.getElementById('maincolor');
+var colorone = document.getElementById('colorone');
+var colortwo = document.getElementById('colortwo');
+var colorthree = document.getElementById('colorthree');
+var colorfour = document.getElementById('colorfour');
+var colorfive = document.getElementById('colorfive');
+var colorsix = document.getElementById('colorsix');
+var colorblocks_arr = [colorone, colortwo, colorthree,
+  colorfour, colorfive, colorsix];
+var colors_rgb = [];
+var start = document.getElementById('start');
+var winner = '';
 
-var play_again = document.getElementById('clicker');
-
-play_again.addEventListener('click', getRandomColor);
+start.addEventListener('click', getRandomColor);
 function getRandomColor () {
-  var min = 1;
-  var max = 250;
-  var color_arr = [];
-  for (var i = 0; i < 3; i++) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    var number = Math.floor(Math.random() * (max - min)) + min;
-    color_arr.push(number);
+  for (var b = 0; b < 6; b++) {
+    var min = 1;
+    var max = 250;
+    var color_arr = [];
+    for (var i = 0; i < 3; i++) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      var number = Math.floor(Math.random() * (max - min)) + min;
+      color_arr.push(number);
+    }
+    colors_rgb.push(color_arr);
   }
-  header.textContent = 'RGB(' + color_arr.toString() + ')';
+  makeColors();
+}
+// header.textContent = 'RGB(' + color_arr.toString() + ')';
+function makeColors () {
+  var win_color = colors_rgb[Math.floor(Math.random() * colors_rgb.length)];
+  header.textContent = 'rgb(' + win_color + ')';
+  for (var i = 0; i < colorblocks_arr.length; i++) {
+    colorblocks_arr[i].style.backgroundColor = 'rgb(' + colors_rgb[i] + ')';
+  }
+}
+
+for (var i = 0; i < colorblocks_arr.length; i++) {
+  colorblocks_arr[i].addEventListener('click', check);
+
+  function check () {
+    var winner = header.textContent;
+    var maybs_winner = event.target.style.backgroundColor.split(' ').join('');
+    console.log(winner);
+    console.log(maybs_winner);
+    if (maybs_winner == winner) {
+        alert('Wow you won!!');
+    }else {
+        alert('keep playing guess again');
+    }
+  }
 }
